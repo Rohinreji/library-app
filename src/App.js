e7d933c9662e631505de7bad41ca2a08ffcf1acd
rohinreji <rohinreji65@gmail.com>
2024-08-07 10:13:01 +0530

    pull

--- src/App.js
+++ src/App.js
@@ -1,37 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
- import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Loginpage from './component/student/Loginpage';
-import Signin from './component/student/Signin';
-import Admin from './Components/admin/Admin'; 
-import CommonNavbar from './Components/common/commonNavbar/commonNavbar';
-import Footer from './Components/common/footer/footer';
-import LandingPage from './Components/common/landingPage/landingPage';
+import Loginpage from "./component/student/Loginpage";
+import Signin from "./component/student/Signin";
+import Admin from "./Components/admin/Admin";
+import CommonNavbar from "./Components/common/commonNavbar/commonNavbar";
+import Footer from "./Components/common/footer/footer";
+import LandingPage from "./Components/common/landingPage/landingPage";
 
 function App() {
-  return (
-  
-<BrowserRouter>
-    <div className="App">
-    <Routes>
-      <Route path='/registration' element={<Loginpage/>}/>
-      <Route path='/login' element={<Signin/>}/>
-      <Route path='/admin' element={<Admin/>}/>
-      {/* common */}
-      <Route path='/commonNavbar' element={<CommonNavbar/>}/>
-      <Route path='/footer' element={<Footer/>}/>
-      <Route path='/landingPage' element={<LandingPage/>}/>
-
-
-    </Routes>
-      
-    
-    </div>
+  return ( <>
+    <BrowserRouter basename="open_library">
+      <Routes>
+        <Route path="/registration" element={<Loginpage />} />
+        <Route path="/login" element={<Signin />} />
+        <Route path="/admin" element={<Admin />} />
+        {/* common */}
+        <Route path="/commonNavbar" element={<CommonNavbar />} />
+        <Route path="/footer" element={<Footer />} />
+        <Route path="/landingPage" element={<LandingPage />} />
+      </Routes>
     </BrowserRouter>
-  );
+  </>
+  )
 }
 
 export default App;

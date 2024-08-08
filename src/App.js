20b25573a15485c51a419eec57f7f02c397171e9
rohinreji <rohinreji65@gmail.com>
2024-08-08 12:53:58 +0530

    etlogin,student login,admin login

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Loginpage from "./Components/student/Loginpage";
 import Signin from "./Components/student/Signin";
-import Admin from "./Components/admin/Admin";
 import CommonNavbar from "./Components/common/commonNavbar/commonNavbar";
 import Footer from "./Components/common/footer/footer";
 import LandingPage from "./Components/common/landingPage/landingPage";
@@ -14,6 +13,9 @@ import { StudentNav } from "./Components/student/studentHome/studentNav/studentN
 import StudentHomeDetails from "./Components/student/studentHome/studentHomeDetail/studentHomeDetail";
 import { StudentHome } from "./Components/student/studentHome/studentHome";
 import { StudentSignUp } from "./Components/student/studentSignUp/studentSignUp";
+import AdminLogin from "./Components/admin/adminLogin";
+import StudentLogin from "./Components/student/studentLogin/studentLogin";
+import TutorLogin from "./Components/tutor/tutorLogin/tutorLogin";
 
 function App() {
   return (
@@ -23,9 +25,9 @@ function App() {
           <Route path="/" element={<LandingPage />} />
 
           {/* student */}
-          <Route path="/registration" element={<Loginpage/> } />
+          <Route path="/registration" element={<Loginpage />} />
           <Route path="/login" element={<Signin />} />
-          <Route path="/admin" element={<Admin />} />
+
           <Route path="/student/navbar" element={<StudentNav />} />
           <Route
             path="/student/home-details"
@@ -33,10 +35,15 @@ function App() {
           />
           <Route path="/student/home" element={<StudentHome />} />
           <Route path="/student/signup" element={<StudentSignUp />} />
+          <Route path="studentLogin" element={<StudentLogin />} />
           {/* common */}
           <Route path="/commonNavbar" element={<CommonNavbar />} />
           <Route path="/footer" element={<Footer />} />
           <Route path="/landingPage" element={<LandingPage />} />
+          {/* admin */}
+          <Route path="/adminLogin" element={<AdminLogin />} />
+          {/* tutor */}
+          <Route path="/tutorLogin" element={<TutorLogin/>}/>
           {/* 404 page */}
           <Route path="/*" element={<h1>The page not available</h1>} />
         </Routes>

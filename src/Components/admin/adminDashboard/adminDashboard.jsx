84b8560d57211984661a4f5a16e140994ce02b4d
rohinreji <rohinreji65@gmail.com>
2024-10-11 11:44:37 +0530

    initial push

--- src/Components/admin/adminDashboard/adminDashboard.jsx
+++ src/Components/admin/adminDashboard/adminDashboard.jsx
@@ -6,12 +6,12 @@ import { AdminViewReturnReq } from "../adminViewReturnReq/adminViewReturnReq";
 import { AdminSideBar } from "../adminSideBar/adminSidBar";
 import { AdminAddProduct } from "../adminAddProduct/adminAddProduct";
 import { AdminViewRentedBooks } from "../adminViewRentedBook/adminViewRentedBooks";
-<<<<<<< HEAD
+
 import { AdminViewStudentRequest } from "../adminViewStudentRequest/adminViewStudentRequest";
 import { AdminViewApprovedStudent } from "../adminViewApprovedStudent/adminViewApprovedStudent";
-=======
+
 import { AdminViewAllBook } from "../adminViewAllBook/adminViewAllBooks";
->>>>>>> 6348941f21f8a41fefbd6c249c9526d0bf6ea4de
+
 
 export const AdminDashboard = () => {
   const [selectedpage, setSelectedPage] = useState("adminViewBooks");
@@ -27,7 +27,7 @@ setSelectedPage("adminViewBooks")
   return (
     <div className="row">
       <div className="col-2">
-<<<<<<< HEAD
+
         <AdminSideBar changeSelectedPage={changeSelectedPage} />
       </div>
       <div className="col-10">
@@ -41,19 +41,7 @@ setSelectedPage("adminViewBooks")
         {selectedpage === "approvedStudent" && <AdminViewApprovedStudent />}
       </div>
     </div>
-=======
-        <AdminSideBar changeSelectedPage={changeSelectedPage}/>
-        </div>
-        <div className="col-10">
-          {selectedpage === "adminViewBooks" && <AdminViewAllBook/>} 
-            {selectedpage === "adminAddBook" && <AdminAddProduct redirectToAdminViewAllBook={redirectToAdminViewAllBook} />}
-          {selectedpage === "approveTutor" && <AdminApproveTutor />}
-          {selectedpage === "viewAllTutor" && <AdminViewApproveTutor />}
-          {selectedpage === "approveRentOfTutor" && <AdminViewAllRental />}
-          {selectedpage === "viewRentedBooksByTutor"&& <AdminViewRentedBooks/>} 
-          {selectedpage === "tutorReturnReq" && <AdminViewReturnReq />}
-        </div>
-  </div>
->>>>>>> 6348941f21f8a41fefbd6c249c9526d0bf6ea4de
+
+   
   );
 };
--- src/Components/admin/adminViewApprovedTutor/adminViewApprovedTutor.jsx
+++ src/Components/admin/adminViewApprovedTutor/adminViewApprovedTutor.jsx
@@ -1,28 +1,19 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Table from "react-bootstrap/Table";
-<<<<<<< HEAD
-export const AdminViewApproveTutor = () => {
-  const [data, setData] = useState([]);
-
-=======
 import { IoSearch } from "react-icons/io5";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 export const AdminViewApproveTutor = () => {
   const [data, setData] = useState([]);
   const [fixedData, setFixedData] = useState([]);
->>>>>>> 6348941f21f8a41fefbd6c249c9526d0bf6ea4de
   const getData = async () => {
     try {
       const response = await axios.get(
         "http://localhost:3005/viewAllApprovedTutors"
       );
       setData(response.data.data);
-<<<<<<< HEAD
-=======
       setFixedData(response.data.data);
->>>>>>> 6348941f21f8a41fefbd6c249c9526d0bf6ea4de
     } catch (error) {
       console.log(error);
     }
@@ -32,11 +23,6 @@ export const AdminViewApproveTutor = () => {
   useEffect(() => {
     getData();
   }, []);
-<<<<<<< HEAD
-  return (
-    <div>
-      <h2>Approved tutors</h2>
-=======
 
   const handleSearch = (e) => {
     e.preventDefault();
@@ -68,7 +54,6 @@ export const AdminViewApproveTutor = () => {
           <IoSearch />
         </InputGroup.Text>
       </InputGroup>
->>>>>>> 6348941f21f8a41fefbd6c249c9526d0bf6ea4de
       <Table striped bordered hover className="adminApproveTutor-table">
         <thead>
           <tr>

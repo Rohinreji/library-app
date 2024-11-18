6e866d4f3e7d48d6b92df2dcf663b02b65b03fb5
manu006m <manu006m@gmail.com>
2024-11-18 10:45:45 +0530

    manu

--- src/Components/admin/adminSideBar/adminSidBar.jsx
+++ src/Components/admin/adminSideBar/adminSidBar.jsx
@@ -5,31 +5,68 @@ export const AdminSideBar = ({ changeSelectedPage }) => {
   const [rentProductByTutor, setRentProductByTutor] = useState(false);
   const [viewStudent, setViewStudent] = useState(false);
   const [studentRentProduct, setStudentRentProduct] = useState(false);
-
+  const [viewUser, setViewUser] = useState(false);
+  const [rental, setRental] = useState(false);
+  const [retunReq, setReturnReq] = useState(false);
   const toggleViewTutor = () => {
     setViewTutorDD(!viewTutorDD);
     setRentProductByTutor(false);
     setViewStudent(false);
     setStudentRentProduct(false);
+    setViewUser(true);
   };
   const toggleViewStudents = () => {
     setViewStudent(!viewStudent);
     setViewTutorDD(false);
     setRentProductByTutor(false);
     setStudentRentProduct(false);
+    setViewUser(true);
   };
   const toggleTutorRentProduct = () => {
     setRentProductByTutor(!rentProductByTutor);
     setViewTutorDD(false);
     setViewStudent(false);
     setStudentRentProduct(false);
+    setViewUser(false);
   };
   const toggleStudentRentProduct = () => {
     setStudentRentProduct(!studentRentProduct);
     setViewTutorDD(false);
     setViewStudent(false);
     setRentProductByTutor(false);
+    setViewUser(false);
   };
+
+  const toggleViewUsers = () => {
+    setViewUser(!viewUser);
+    setStudentRentProduct(false);
+    setViewTutorDD(false);
+    setViewStudent(false);
+    setRentProductByTutor(false);
+    setRental(false);
+    setReturnReq(false);
+  };
+
+  const toggleRentals = () => {
+    setRental(!rental);
+    setViewUser(false);
+    setStudentRentProduct(false);
+    setViewTutorDD(false);
+    setViewStudent(false);
+    setRentProductByTutor(false);
+    setReturnReq(false);
+  };
+
+  const toggleRetunReq = () => {
+    setReturnReq(!retunReq);
+    setViewUser(false);
+    setStudentRentProduct(false);
+    setViewTutorDD(false);
+    setViewStudent(false);
+    setRentProductByTutor(false);
+    setRental(false);
+  };
+
   return (
     <div>
       <div className="admin-sidebar">
@@ -49,98 +86,118 @@ export const AdminSideBar = ({ changeSelectedPage }) => {
           >
             chat with tutor
           </li>
-          <li onClick={toggleViewTutor}>View tutors</li>
-          {viewTutorDD && (
+
+          <li onClick={toggleViewUsers}>View Users</li>
+          {viewUser && (
             <div className="shadow mx-3">
-              <li
-                onClick={() => {
-                  changeSelectedPage("approveTutor");
-                }}
-              >
-                approve tutor
-              </li>
-              <li
-                onClick={() => {
-                  changeSelectedPage("viewAllTutor");
-                }}
-              >
-                View all tutors
-              </li>
+              <li onClick={toggleViewTutor}> tutors</li>
+              {viewTutorDD && (
+                <div className="shadow mx-3">
+                  <li
+                    onClick={() => {
+                      changeSelectedPage("approveTutor");
+                    }}
+                  >
+                    Approve 
+                  </li>
+                  <li
+                    onClick={() => {
+                      changeSelectedPage("viewAllTutor");
+                    }}
+                  >
+                    View all 
+                  </li>
+                </div>
+              )}
+              <li onClick={toggleViewStudents}> Students</li>
+              {viewStudent && (
+                <div className="shadow mx-3">
+                  <li
+                    onClick={() => {
+                      changeSelectedPage("studentReq");
+                    }}
+                  >
+                    Approve 
+                  </li>
+                  <li
+                    onClick={() => {
+                      changeSelectedPage("approvedStudent");
+                    }}
+                  >
+                    View all 
+                  </li>
+                </div>
+              )}
             </div>
           )}
-          <li onClick={toggleViewStudents}>View Students</li>
-          {viewStudent && (
+
+          <li onClick={toggleRentals}>Rentals</li>
+          {rental && (
+            <div className="shadow mx-3">
+              <li onClick={toggleStudentRentProduct}>Student </li>
+              {studentRentProduct && (
+                <div className="shadow mx-3">
+                  <li
+                    onClick={() => {
+                      changeSelectedPage("viewStdRentals");
+                    }}
+                  >
+                    approve rent
+                  </li>
+                  <li
+                    onClick={() => {
+                      changeSelectedPage("viewStdRentedBooks");
+                    }}
+                  >
+                    view rented books
+                  </li>
+                </div>
+              )}
+
+              <li onClick={toggleTutorRentProduct}>tutor </li>
+              {rentProductByTutor && (
+                <div className="shadow mx-3">
+                  <li
+                    onClick={() => {
+                      changeSelectedPage("approveRentOfTutor");
+                    }}
+                  >
+                    approve rent
+                  </li>
+                  <li
+                    onClick={() => {
+                      changeSelectedPage("viewRentedBooksByTutor");
+                    }}
+                  >
+                    view rented books
+                  </li>
+                </div>
+              )}
+            </div>
+          )}
+
+          <li onClick={toggleRetunReq}>Return Request</li>
+
+          {retunReq && (
             <div className="shadow mx-3">
               <li
                 onClick={() => {
-                  changeSelectedPage("studentReq");
+                  changeSelectedPage("tutorReturnReq");
                 }}
               >
-                Approve students
+                Tutor
               </li>
+
               <li
                 onClick={() => {
-                  changeSelectedPage("approvedStudent");
+                  changeSelectedPage("studentReturnRequest");
                 }}
               >
-                View all Students
+                Student
               </li>
             </div>
           )}
 
-          <li onClick={toggleStudentRentProduct}>Student rent product</li>
-          {studentRentProduct && (
-            <div className="shadow mx-3">
-              <li
-                onClick={() => {
-                  changeSelectedPage("viewStdRentals");
-                }}
-              >
-                approve rent
-              </li>
-              <li
-                onClick={() => {
-                  changeSelectedPage("viewStdRentedBooks");
-                }}
-              >
-                view rented books
-              </li>
-            </div>
-          )}
-
-          <li
-            onClick={() => {
-              changeSelectedPage("studentReturnRequest");
-            }}
-          >
-            Student Return Request
-          </li>
-          <li onClick={toggleTutorRentProduct}>tutor rent product</li>
-          {rentProductByTutor && (
-            <div className="shadow mx-3">
-              <li
-                onClick={() => {
-                  changeSelectedPage("approveRentOfTutor");
-                }}
-              >
-                approve rent
-              </li>
-              <li
-                onClick={() => {
-                  changeSelectedPage("viewRentedBooksByTutor");
-                }}
-              >
-                view rented books
-              </li>
-            </div>
-          )}
-          <li
-            onClick={() => {
-              changeSelectedPage("tutorReturnReq");
-            }}
-          >
-            Tutor return request
-          </li>
           <li
             className="text-danger fs-3"
             onClick={() => {
